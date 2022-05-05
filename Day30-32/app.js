2f1f578afcfad20d6c3911537079de51d3a899e9
leesungeon <devcez82@gmail.com>
2022-05-05 15:14:20 +0900

    Day30

---
+++ Day30-32/app.js
@@ -0,0 +1,19 @@
+/*let age = 32
+let greetingText = "Hi I'm Max!";
+alert(greetingText);
+alert(greetingText);
+greetingText = "Hi I'm really Max!";
+alert(greetingText);
+alert(age);
+*/
+
+let age = 32;
+let userName = 'Max';
+/*let hobby1;
+let hobby2;*/
+let hobbies = ['Sport', 'Cooking', 'Reading'];
+let job = {title:'Developer', place:'New York', salary: 50000};
+
+alert(hobbies[0]);
+alert(hobbies[1]);
+alert(hobbies[2]);
