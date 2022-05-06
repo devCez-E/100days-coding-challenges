75cc1a1df3a949a969175d2003b72c5246f4c97d
leesungeon <devcez82@gmail.com>
2022-05-06 15:12:09 +0900

    Day33

---
+++ Day33-36/app.js
@@ -0,0 +1,8 @@
+//console.log(window);
+//alert();
+//window.alert();
+
+// DOM
+// Document Object Model
+//console.dir(document);
+document.body.children[1].children[0].href = 'https://google.com';
