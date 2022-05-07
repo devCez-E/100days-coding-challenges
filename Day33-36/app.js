47ccf99c017ca1fd23fd862cb0b274883627c9c9
leesungeon <devcez82@gmail.com>
2022-05-07 17:34:00 +0900

    Day34

--- Day33-36/app.js
+++ Day33-36/app.js
@@ -5,4 +5,23 @@
 // DOM
 // Document Object Model
 //console.dir(document);
-document.body.children[1].children[0].href = 'https://google.com';
+//document.body.children[1].children[0].href = 'https://google.com';
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('#external-link');
+anchorElement.href = 'https://academind.com';
+
+anchorElement = document.querySelector('p a');
+anchorElement.href = 'https://www.naver.com';
+
+
+// Add an Element
+// 1. Create the new element
+let newAnchorElement = document.createElement('a');
+newAnchorElement.textContent = "Link";
+newAnchorElement.href = "https://google.com";
+// 2. Get access to the parent element that should hold the new element
+let firstParagraph = document.querySelector('p');
+// 3. Insert the new element into the parent element content
+firstParagraph.append(newAnchorElement);
---
+++ Day33-36/exercise.js
@@ -0,0 +1,30 @@
+// Exercise Time!
+
+// 1. Select the <h1> element by "drilling into the DOM" and 
+//    save it in a variable with a name of your choice
+let title = document.body.children[0];
+console.dir(title);
+
+// 2. Use the variable from (1) and get access to the "parent"
+//    element of the stored <h1> element (i.e. to the <body> element)
+let body = title.parentElement;
+console.dir(body);
+//    BONUS: Try using the variable from (1) to get access to the 
+//    sibling element (i.e. the <p> element next to the <h1> element)
+let brother = title.nextElementSibling;
+console.dir(brother);
+
+// 3. Select the <h1> element with getElementById and store in
+//    the same or a new variable (up to you)
+let title_id = document.getElementById('title');
+console.dir(title_id);
+
+// 4. Select the second <p> element with querySelector (you might
+//    need to add something in the HTML code, e.g. a class) 
+//    and store it in a new variable with a name of your choice
+let title_q = document.querySelector('.main-text');
+console.dir(title_q);
+
+// 5. BONUS TASK: Try changing the text content of the <p> element
+//    you selected in (4) and set it to any other text of your choice
+title_q.textContent = "Change Text Yeppi!!";
