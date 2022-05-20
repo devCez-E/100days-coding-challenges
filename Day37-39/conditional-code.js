aea967cd46af353c809ef0bf1a30d8e1cc9e28e1
leesungeon <devcez82@gmail.com>
2022-05-20 15:59:10 +0900

    Day37

---
+++ Day37-39/conditional-code.js
@@ -0,0 +1,23 @@
+const myName = 'Max';
+
+if(true){
+    console.log('Hello!');
+}
+
+console.log(5 == 5);
+console.log('5' == 5);
+console.log(5 === 5);
+console.log('5' === 5);
+
+console.log(5 != 5);
+console.log('5' != 5);
+console.log(5 !== 5);
+console.log('5' !== 5);
+
+console.log(2 === 2 || 2 === 3 && 5 === 6);
+console.log(2 === 2 || 2 === 2 && 5 === 6);
+console.log((2 === 2 || 2 === 2) && 5 === 6);
+
+if(myName ==='Max'){
+    console.log('Hello!');
+}
